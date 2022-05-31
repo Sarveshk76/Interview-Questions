7818f08f8c5fadcb2950ae9107d46b3b26f2419c
Sarvesh S. Kulkarni <78719645+Sarveshk76@users.noreply.github.com>
2022-05-31 23:43:30 +0530

    Update Two number addition.py

--- Two number addition.py
+++ Two number addition.py
@@ -1,11 +1,16 @@
 arr = [1,2,3,4,5]
 target = 4
 num={}
-def TwoNum(arr, target):
+def twosum(arr, target):
     for i in range(len(arr)):
         if arr[i] not in num.keys():
             num[target-arr[i]]=i
         else:
             print(num)
             return [target-arr[i],arr[i]]
-print(TwoNum(arr,target))
+print(twosum(arr,target))
+
+#Using List Comprehension
+def twoSum(self, nums: List[int], target: int) -> List[int]:
+        res= [[i,j] for i in range(len(nums)-1) for j in range(i+1,len(nums)) if nums[i]+nums[j]==target]
+        return res[0]
