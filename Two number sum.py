3d789af448f8a682301e3c489844dc41d4ed6802
Sarvesh S. Kulkarni <78719645+Sarveshk76@users.noreply.github.com>
2022-06-02 11:33:45 +0530

    Update and rename Two number addition.py to Two number sum.py

--- Two number sum.py
+++ Two number sum.py
@@ -1,16 +1,16 @@
 arr = [1,2,3,4,5]
 target = 4
 num={}
-def twosum(arr, target):
+def two_num_sum(arr, target):
     for i in range(len(arr)):
         if arr[i] not in num.keys():
             num[target-arr[i]]=i
         else:
             print(num)
             return [target-arr[i],arr[i]]
-print(twosum(arr,target))
+print(two_num_sum(arr,target))
 
 #Using List Comprehension
-def twoSum(self, nums: List[int], target: int) -> List[int]:
+def two_num_sum(arr,target):
         res= [[i,j] for i in range(len(nums)-1) for j in range(i+1,len(nums)) if nums[i]+nums[j]==target]
         return res[0]
